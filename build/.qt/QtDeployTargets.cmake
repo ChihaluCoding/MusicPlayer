361159db33841726bc375c15afc59f6ca3a1ddae
Chihalu <m.chihiro0314@gmail.com>
2025-12-19 14:05:44 +0900

    Remove autogenerated files and binaries from the MusicPlayer build directory, including mocs_compilation.cpp, Qt6 libraries, FFmpeg libraries, and translation files.

--- build/.qt/QtDeployTargets.cmake
+++
@@ -1,3 +0,0 @@
-set(__QT_DEPLOY_TARGET_MusicPlayer_FILE I:/Programming/PF/minecraft mod/MusicPlayer/build/MusicPlayer.exe)
-set(__QT_DEPLOY_TARGET_MusicPlayer_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_MusicPlayer_RUNTIME_DLLS C:/Qt/6.10.1/mingw_64/bin/Qt6Widgets.dll;C:/Qt/6.10.1/mingw_64/bin/Qt6Multimedia.dll;C:/Qt/6.10.1/mingw_64/bin/Qt6Gui.dll;C:/Qt/6.10.1/mingw_64/bin/Qt6Network.dll;C:/Qt/6.10.1/mingw_64/bin/Qt6Concurrent.dll;C:/Qt/6.10.1/mingw_64/bin/Qt6Core.dll)
